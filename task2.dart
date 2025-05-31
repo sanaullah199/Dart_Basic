ea2752dd80cbebd0790f6d4cc8ade525116ec668
sanaullah199 <sanau19191@gmail.com>
2025-05-31 04:44:39 -0700

    Add files via upload

---
+++ task2.dart
@@ -0,0 +1,24 @@
+ void main(){
+    //hard core 
+    int num1 = 20;
+    int num2 = 40;
+    // now i need to perform all arithmetic operation
+    // ignore: unused_local_variable
+    int addition = num1 + num2;
+    int subtraction = num1 - num2;
+    int multiplication = num1* num2;
+ // ignore: unused_local_variable
+ double division = num1 / num2;
+ // ignore: unused_local_variable
+ int modulus =  num1 % num2;
+ int integerDevision = num1 ~/ num2;
+ //so i declare and initializtion of all arithmetic operation 
+ print('Number 1: $num1');
+ print('Number 2: $num2');
+ print('Addition (+) : $addition');
+ print('subtraction (-) : $subtraction');
+ print('Multiplication (*): $multiplication'); 
+print('Division (/): $division');
+print('Modulus (%):$modulus');
+print('IntegerDevision(~/): $integerDevision');
+ }
