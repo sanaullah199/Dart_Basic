77fcf65b47ce4814b916338668545ea5034558d4
sanaullah199 <sanau19191@gmail.com>
2025-05-31 00:32:56 -0700

    Add files via upload

---
+++ task1.dart/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_local_variable
+
+void main(){
+//declare variable
+String myName = 'Ali'; //hold character and statements
+int age = 30; // int is a data type which is used to store integer values
+String city ='peshawar'; // final varaible its value cannot be change once initialized
+// now i wanna print the result , so for that we using print function to display result
+print('hello $myName! you are $age years old and you live in $city.');
+}
